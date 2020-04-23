47a8856f6780d66cf8d3f59388146ef1a91e18c5
Kishor Rana <30983917+kshrana@users.noreply.github.com>
2020-04-23 11:28:39 +0200

    Add Netlify CMS

--- gatsby-config.js
+++ gatsby-config.js
@@ -79,5 +79,11 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-netlify-cms`,
+      options: {
+        modulePath: `${__dirname}/src/cms/cms.js`,
+      },
+    },
   ],
 }
---
+++ src/cms/cms.js
@@ -0,0 +1,4 @@
+import CMS from 'netlify-cms-app'
+
+// CMS.registerPreviewTemplate('my-template', MyTemplate)
+CMS.init()
